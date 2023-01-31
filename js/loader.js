2a0c269c4d817a706c3b070a6fb861b2268ae014
carpicoder <webcarpi@gmail.com>
2023-01-31 00:17:20 -0300

    Agregué loader

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#spinner").style.visibility = "visible";
+    } else {
+        document.querySelector("body").style.visibility = "visible";
+        document.querySelector("#spinner").style.visibility = "hidden";
+        document.querySelector("#spinner").style.opacity = "0";
+        document.querySelector("#spinner").style.transform = "translateY(100%)";
+        setTimeout(() => {
+            document.querySelector("#spinner").style.display = "none";
+        }, 2000);
+    }
+};
